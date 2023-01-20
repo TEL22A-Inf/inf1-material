1b1c97962f728eb49602a616a927d67e975e683d
Reiner Hüchting <dev@reinerhuechting.de>
2023-01-20 12:57:25 +0100

    Beispiele zu 2D-Listen hinzugefügt.

---
+++ code-vorlesung/2023-01-20/matrizen/matrizen_test.go
@@ -0,0 +1,28 @@
+package matrizen
+
+import "fmt"
+
+func ExampleMatrizen() {
+	// Erzeuge eine 3x3-Matrix aus Zahlen
+	m1 := [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
+
+	fmt.Println(m1)
+	fmt.Println(m1[0])
+	fmt.Println(m1[0][0])
+	fmt.Println(m1[2][0])
+
+	for _, row := range m1 {
+		fmt.Println(row)
+	}
+	for _, row := range m1 {
+		for pos, element := range row {
+			fmt.Print(element)
+			if pos < len(row)-1 {
+				fmt.Print(",")
+			}
+		}
+		fmt.Println()
+	}
+
+	// Output:
+}
